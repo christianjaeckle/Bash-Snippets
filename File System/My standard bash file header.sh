f1dd8fef29c8b992f9e282dbf11eb19c10bcc592
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-09-28 15:57:10 +0200

    Create My standard bash file header.sh

---
+++ File System/My standard bash file header.sh
@@ -0,0 +1,8 @@
+Script_FileName=$(basename "$0")
+Script_Path=dirname "$(realpath $0)"
+Script_LogDate=date +"%Y%m%d"
+Script_LogPath=$Script_Path/$Script_LogDate
+"\%Script_LogDate% - $Script_FileName.log"
+Script_LogFilePath=$Script_LogPath/$Script_LogDate - $Script_FileName.log
+
+echo '$(date +"%Y%m%d %H:%M:%S") - Info - Start of script: $0'
